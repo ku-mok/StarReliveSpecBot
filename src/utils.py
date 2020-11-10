1dac1bde6afd2c7e9f22a043db058d50e3e81173
mok <mok>
2020-11-10 20:19:19 +0900

    テキスト整形が戻り値返し忘れを修正

--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,16 @@ import pandas as pd
 
 
 def df_all_row_to_str(df: pd.DataFrame):
-    text = ""
+    text = "```\n"
     for row in df.itertuples():
         text += f"{row[1]}|{row[2]}|{row[3]}\n"
+    text += "```"
+    return text
 
 
 def df_two_row_to_str(df: pd.DataFrame):
-    text = ""
+    text = "```\n"
     for row in df.itertuples():
         text += f"{row[2]}|{row[3]}\n"
+    text += "```"
+    return text
