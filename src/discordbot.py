b3026079c2d9860c75b8aec3d4b94429f5c35964
mok <mok>
2020-11-10 20:09:11 +0900

    スクレイピング関数の呼び出し実装

--- src/discordbot.py
+++ src/discordbot.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 import os
+from wikiscraping import get_char_info
+from utils import df_all_row_to_str, df_two_row_to_str
 import traceback
 
 bot = commands.Bot(command_prefix='/')
@@ -15,7 +17,18 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def starrelive(ctx, arg):
+async def starrelive(ctx, command, arg):
+    command_list = {"all": 0, "act": 1, "skill": 2, "status": 3}
+    if command not in command_list:
+        await ctx.send("コマンドが見つかりません")
+    elif command == "all":
+        await ctx.send(arg + "の性能は")
+        result = get_char_info(arg)
+        await ctx.send(df_all_row_to_str(result))
+    elif (command_list[command] >= 1 or command_list[command] <= 3):
+        result = get.get_char_info(arg, command)
+        await ctx.send(df_two_row_to_str(result))
+
     await ctx.send(arg)
 
 
